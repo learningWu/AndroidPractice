28c7fdda6297a15d9ce16ad4ef51d394c9571ee7
EddieZX <1600428416@qq.com>
2019-06-15 21:39:09 +0800

    add constraintlayout

---
+++ app/src/main/java/com/example/dzj/android_practice/constraintlayoutdemo/ConstraintLayoutTestActivity.kt
@@ -0,0 +1,13 @@
+package com.example.dzj.android_practice.constraintlayoutdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dzj.android_practice.R
+
+class ConstraintLayoutTestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constrain_layout_test)
+    }
+}
--- app/src/main/java/com/example/dzj/android_practice/networkdemo/OkHttpTest.kt
+++ app/src/main/java/com/example/dzj/android_practice/networkdemo/OkHttpTest.kt
@@ -1,8 +1,6 @@
 package com.example.dzj.android_practice.networkdemo
 
-import okhttp3.Interceptor
-import okhttp3.OkHttpClient
-import okhttp3.Response
+import okhttp3.*
 import okhttp3.internal.Util.hostHeader
 import okhttp3.internal.Version
 import okhttp3.internal.http.HttpHeaders
@@ -10,10 +8,27 @@ import okhttp3.internal.http.RealResponseBody
 import okhttp3.logging.HttpLoggingInterceptor
 import okio.GzipSource
 import okio.Okio
+import java.io.IOException
 
 class OkHttpTest {
 
     fun main() {
+        val newCall = okHttpClient.newCall(Request.Builder()
+                .url("")
+                .build())
+        //同步
+        newCall.execute()
+        //异步
+        newCall.enqueue(object : Callback {
+
+            override fun onFailure(call: Call, e: IOException) {
+
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+
+            }
+        })
     }
 
     companion object OkHttpClient {
@@ -22,13 +37,12 @@ class OkHttpTest {
                 field.setLevel(HttpLoggingInterceptor.Level.BODY)
                 return field
             }
-        private set(value) = Unit
+            private set(value) = Unit
 
         val okHttpClient = OkHttpClient()
                 .newBuilder()
                 .addInterceptor(logInterceptor)
                 .build()
-
     }
 
     class MyInterceptor : Interceptor {
