2e60803b6bead9dadfeff135d5e3e2fc2c2e3784
EddieZX <1600428416@qq.com>
2019-08-13 10:49:10 +0800

    增加自动编译jni到jniLibs的gradle

--- app/src/main/obj/local/armeabi-v7a/objs/jnitest/*.o.d
+++ app/src/main/obj/local/armeabi-v7a/objs/jnitest/*.o.d
@@ -1,4 +1,5 @@
-./obj/local/armeabi-v7a/objs/jnitest/./*.o: jni/./JniTest.cpp \
-  jni/com_example_dzj_android_practice_jnidemo_JniTest.h
+/Users/dzj/Documents/MyProgram/androidpractice/app/src/main/obj/local/armeabi-v7a/objs/jnitest/./*.o: \
+  /Users/dzj/Documents/MyProgram/androidpractice/app/src/main/jni/./JniTest.cpp \
+  /Users/dzj/Documents/MyProgram/androidpractice/app/src/main/jni/com_example_dzj_android_practice_jnidemo_JniTest.h
 
-jni/com_example_dzj_android_practice_jnidemo_JniTest.h:
+/Users/dzj/Documents/MyProgram/androidpractice/app/src/main/jni/com_example_dzj_android_practice_jnidemo_JniTest.h:
